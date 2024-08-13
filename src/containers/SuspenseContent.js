0c9d39c71ae75ad5eb9566fbb6efaab3a4750f15
mphstar <bintang.simdig20@gmail.com>
2024-08-13 10:33:54 +0700

    feat: change loading animation

--- src/containers/SuspenseContent.js
+++ src/containers/SuspenseContent.js
@@ -1,9 +1,22 @@
-function SuspenseContent(){
-    return(
-        <div className="w-full h-screen text-gray-300 dark:text-gray-200 bg-base-100">
-            Loading...
-        </div>
-    )
+import Lottie from "react-lottie";
+import * as animationData from "./loading.json";
+
+function SuspenseContent() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="w-full h-screen bg-white flex flex-col justify-center items-center">
+      <Lottie options={defaultOptions} height={200} width={200} />;
+      <span className="text-gray-800 font-semibold">Wait a moment...</span>
+    </div>
+  );
 }
 
-export default SuspenseContent
+export default SuspenseContent;
