a31ca4b7a6193fe5e4e6eadc47aa8554cd9e08a2
mphstar <bintang.simdig20@gmail.com>
2024-09-17 10:46:38 +0700

    feat: [modal product & service] slicing page

--- src/features/company/product-services/ProductService.jsx
+++ src/features/company/product-services/ProductService.jsx
@@ -4,14 +4,354 @@ import FilterCard from "../../../components/Cards/FilterCard";
 import { IoSearchOutline } from "react-icons/io5";
 import { GrPowerReset } from "react-icons/gr";
 import CustomTable from "../../../components/TablePage/CustomTable";
-import { COLUMN, ROW } from "./dummyData";
 import Paginate from "../../../components/Paginate/Paginate";
 import { TbFileExport, TbFileImport } from "react-icons/tb";
 import { useDispatch } from "react-redux";
 import { openModal } from "../../common/modalSlice";
 import { MODAL_BODY_TYPES } from "../../../utils/globalConstantUtil";
+import { FiEdit } from "react-icons/fi";
+import { MdDeleteOutline } from "react-icons/md";
 
 const ProductService = () => {
+  const COLUMN = [
+    {
+      value: "Name",
+      className: "",
+    },
+    {
+      value: "SKU",
+      className: "",
+    },
+    {
+      value: "Sale Price",
+      className: "",
+    },
+    {
+      value: "Purchase Price",
+      className: "",
+    },
+    {
+      value: "TAX",
+      className: "",
+    },
+    {
+      value: "Category",
+      className: "",
+    },
+    {
+      value: "Unit",
+      className: "",
+    },
+    {
+      value: "Quantity",
+      className: "",
+    },
+    {
+      value: "Type",
+      className: "",
+    },
+    {
+      value: "Action",
+      className: "",
+    },
+  ];
+  
+  const ROW = [
+    [
+      { value: "Women Bag", className: "" },
+      { value: "P100", className: "" },
+      { value: "$350.00", className: "" },
+      { value: "$300.00", className: "" },
+      { value: "CGST SGST", className: "" },
+      { value: "Business profits", className: "" },
+      { value: "PC", className: "" },
+      { value: "101", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Clock", className: "" },
+      { value: "00711", className: "" },
+      { value: "$200.00", className: "" },
+      { value: "$200.00", className: "" },
+      { value: "SGST", className: "" },
+      { value: "Medical", className: "" },
+      { value: "Inch", className: "" },
+      { value: "400", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Oven", className: "" },
+      { value: "TRG520", className: "" },
+      { value: "$500.00", className: "" },
+      { value: "$500.00", className: "" },
+      { value: "CGST", className: "" },
+      { value: "Medical", className: "" },
+      { value: "Inch", className: "" },
+      { value: "701", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Shoes", className: "" },
+      { value: "00152", className: "" },
+      { value: "$5.00", className: "" },
+      { value: "$416.00", className: "" },
+      { value: "SGST Service Tax", className: "" },
+      { value: "Business profits", className: "" },
+      { value: "Meter", className: "" },
+      { value: "-30", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Purse", className: "" },
+      { value: "001452", className: "" },
+      { value: "$410.00", className: "" },
+      { value: "$495.00", className: "" },
+      { value: "SGST Service Tax", className: "" },
+      { value: "Medical", className: "" },
+      { value: "PC", className: "" },
+      { value: "49", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "T-Shirt", className: "" },
+      { value: "BC008A74", className: "" },
+      { value: "$628.00", className: "" },
+      { value: "$338.00", className: "" },
+      { value: "SGST Service Tax", className: "" },
+      { value: "Medical", className: "" },
+      { value: "PC", className: "" },
+      { value: "20", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Car", className: "" },
+      { value: "SKU123", className: "" },
+      { value: "$100.00", className: "" },
+      { value: "$100.00", className: "" },
+      { value: "SGST", className: "" },
+      { value: "Medical", className: "" },
+      { value: "Inch", className: "" },
+      { value: "-20", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Bicycle Clothing", className: "" },
+      { value: "BC008AC1", className: "" },
+      { value: "$10,000.00", className: "" },
+      { value: "$1,000.00", className: "" },
+      { value: "SGST", className: "" },
+      { value: "Clothing", className: "" },
+      { value: "Inch", className: "" },
+      { value: "48", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Teagan Forbes", className: "" },
+      { value: "BC008ACC", className: "" },
+      { value: "$2.00", className: "" },
+      { value: "$866.00", className: "" },
+      { value: "CGST SGST", className: "" },
+      { value: "Clothing", className: "" },
+      { value: "Inch", className: "" },
+      { value: "12", className: "" },
+      { value: "Product", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+    [
+      { value: "Helmet", className: "" },
+      { value: "HL005MET", className: "" },
+      { value: "$50.00", className: "" },
+      { value: "$30.00", className: "" },
+      { value: "CGST SGST", className: "" },
+      { value: "Business profits", className: "" },
+      { value: "Inch", className: "" },
+      { value: "-6", className: "" },
+      { value: "Service", className: "" },
+      {
+        title: "Action",
+        value: (
+          <div className="flex items-center gap-1">
+            <div className="tooltip" data-tip="Edit">
+              <button onClick={() => ModalForm({ title: "Edit Product & Services" })} className="btn btn-square btn-sm bg-blue-400 hover:bg-blue-500 text-white">
+                <FiEdit />
+              </button>
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button className="btn btn-square btn-sm bg-red-400 hover:bg-red-500 text-white">
+                <MdDeleteOutline />
+              </button>
+            </div>
+          </div>
+        ),
+        className: "",
+      },
+    ],
+  ];
+  
+  
+  
+    
   const dispatch = useDispatch();
 
   const ModalImport = ({ title }) => {
@@ -22,6 +362,7 @@ const ProductService = () => {
       })
     );
   };
+
   const ModalForm = ({ title }) => {
     dispatch(
       openModal({
@@ -39,7 +380,7 @@ const ProductService = () => {
         breadcrumb={"Product & Services"}
         actions={
           <div className="flex gap-2">
-            <div className="tooltip" data-tip="Import">
+            <div className="tooltip tooltip-bottom" data-tip="Import">
               <button
                 onClick={() => ModalImport({ title: "Import Product CSV" })}
                 className="btn btn-square btn-primary btn-sm"
@@ -47,7 +388,7 @@ const ProductService = () => {
                 <TbFileImport />
               </button>
             </div>
-            <div className="tooltip" data-tip="Export">
+            <div className="tooltip tooltip-bottom" data-tip="Export">
               <button
                 onClick={() => {}}
                 className="btn btn-square btn-warning btn-sm"
@@ -55,7 +396,7 @@ const ProductService = () => {
                 <TbFileExport />
               </button>
             </div>
-            <div className="tooltip" data-tip="Add Data">
+            <div className="tooltip tooltip-bottom" data-tip="Add Data">
               <button
                 onClick={() =>
                   ModalForm({ title: "Create Product & Services" })
